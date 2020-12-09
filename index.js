7fafbb6cf28cc8e4bfc9e99113e48b9db91be3f0
MalHibou <swperrin51@gmail.com>
2020-12-09 14:43:44 -0600

    solve stretch 2

--- index.js
+++ index.js
@@ -200,16 +200,24 @@ Baskin Robins now offers new flavors, seasonal flavors, and even regional flavor
 from originalFlavors, currentFlavors, seasonalFlavors, and regionalFlavors and store it in an array called randomFlavors.
 
 Use the getRandomFlavors function and new arrays below to do the following:
-    1. Receive the four arrays with all the differnet flavors (originalFlavors is above, the others are below)
+    1. Receive the four arrays with all the different flavors (originalFlavors is above, the others are below)
     2. Randomly pick flavors from all four arrays
-    3. Return a new array called randomFlavors that has a lenght of 31
+    3. Return a new array called randomFlavors that has a length of 31
 
     For example: getRandomFlavors(originalFlavors, newFlavors, seasonalFlavors, regionalFlavors) might return ["Strawberry Cheesecake", "Eggnog,"..."Chocolate"].
 */
 
 
-function getRandomFlavors(/*code here*/){
-    /*code here*/
+function getRandomFlavors (originalFlavors, newFlavors, seasonalFlavors, regionalFlavors) {
+    const allFlavors = originalFlavors.concat(newFlavors, seasonalFlavors, regionalFlavors)
+    const thirtyOne = []
+    while (thirtyOne.length < 31) {
+        let randNum = Math.floor(Math.random() * allFlavors.length)
+        if (!thirtyOne.includes(allFlavors[randNum])) {
+            thirtyOne.push(allFlavors[randNum])
+        }
+    }
+    return thirtyOne
 }
 
 // NEW DATA ARRAYS FOR STRETCH 2 ⬇️
